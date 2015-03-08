02196da4401db01ea880909b92cda2ee5f600a7b
dancingbush <dancingbush@gmail.com>
2015-03-08 17:16:44 +0000

    Initial Commit

---
+++ LabMaternalhaematology/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LabMaternalhaematology
+//
+//  Created by Ciaran Mooney on 08/03/2015.
+//  Copyright (c) 2015 Ciaran Mooney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
